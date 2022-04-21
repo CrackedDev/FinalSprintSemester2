5c5a588b10d608ab5ef1948018e16084dffda3b6
Cody <91859452+CrackedDev@users.noreply.github.com>
2022-04-21 19:28:42 -0230

    Updated Commit. Finished

---
+++ Profile/script.js
@@ -0,0 +1,29 @@
+var ic1 = new ImageCollection([
+  "images/code1.jpg",
+  "images/code2.jpg",
+  "images/code3.jpg",
+  "images/code4.jpg",
+  "images/code5.jpg",
+]);
+
+function ImageCollection(images) {
+  this.images = images;
+  this.i = 0;
+  this.next = function (imgId) {
+    var img = document.getElementById(imgId);
+    this.i++;
+    if (this.i == images.length) this.i = 0;
+    img.src = images[this.i];
+  };
+  this.prev = function (imgId) {
+    var img = document.getElementById(imgId);
+    this.i--;
+    if (this.i <= 0) this.i = images.length - 1;
+    img.src = images[this.i];
+  };
+}
+
+function enterMatrix() {
+  document.getElementById("softbody").style.backgroundImage =
+    "url(./images/matrix.gif)";
+}
